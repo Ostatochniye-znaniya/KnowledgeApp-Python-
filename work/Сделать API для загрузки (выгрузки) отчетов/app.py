e4e7b22294d6d66ccb139cd53b2f3cd29a844afd
yar <yar2022789@gmail.com>
2026-03-12 17:32:10 +0300

    fixed report method

--- work/Сделать API для загрузки (выгрузки) отчетов/app.py
+++ work/Сделать API для загрузки (выгрузки) отчетов/app.py
@@ -4,10 +4,10 @@ from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
 REPORTS = {}
-SPECIALTIES = ()
-PROGRAMS = ()
-PROFESSORS = []
-GROUPS = ()
+SPECIALTIES = ("CS",)                                               #тестовые значения
+PROGRAMS = ("AI",)
+PROFESSORS = ["Ivanov",]
+GROUPS = ("A101",)
 
 
 @app.route('/')
@@ -17,21 +17,23 @@ def index():
 @app.route('/report', methods = ["GET", "POST"])
 def report():
     if request.method == "GET":                                                            #получить/вернуть запрос
-        ...
+        return REPORTS
     if request.method == "POST":
-        report = request.form.get("data")
-        check = report.speciality in SPECIALTIES and report.program in PROGRAMS \
-        and report.professor in PROFESSORS and request.group in GROUPS
+        report_obj = request.json
+        check = report_obj["speciality"] in SPECIALTIES and report_obj["program"] in PROGRAMS \
+        and report_obj["professor"] in PROFESSORS and report_obj["group"] in GROUPS
 
         if check:
-            REPORTS[report["id"]] = report
+            REPORTS[report_obj["id"]] = report_obj
+            print(REPORTS)
+            return REPORTS
         else:
-            return redirect(url_for('report_error'))
+            return REPORTS
                                                                       
-@app.route('/report_error', methods = ["GET, POST"])
-def report_error():                                                                        #выкинуть ошибку о неверном заполнении
+@app.route('/report_error', methods = ["GET"])
+def report_error():
     ...
 
-@app.route('/done', methods = ["POST"])
+@app.route('/done', methods = ["GET"])
 def signed():
     ...                                                                                 #повесить флаг что готово к печати
