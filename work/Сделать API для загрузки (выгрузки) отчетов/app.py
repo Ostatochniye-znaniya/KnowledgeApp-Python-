f60b38545003ea1a9971c6e335f89439c9d318bc
yar <yar2022789@gmail.com>
2026-03-12 14:15:58 +0300

    added logic for post

--- work/Сделать API для загрузки (выгрузки) отчетов/app.py
+++ work/Сделать API для загрузки (выгрузки) отчетов/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
+
 
 app = Flask(__name__)
+
+REPORTS = {}
+SPECIALTIES = ()
+PROGRAMS = ()
+PROFESSORS = []
+GROUPS = ()
+
 
 @app.route('/')
 def index():
@@ -11,9 +19,15 @@ def report():
     if request.method == "GET":                                                            #получить/вернуть запрос
         ...
     if request.method == "POST":
-        ...
+        report = request.form.get("data")
+        check = report.speciality in SPECIALTIES and report.program in PROGRAMS \
+        and report.professor in PROFESSORS and request.group in GROUPS
 
-
+        if check:
+            REPORTS[report["id"]] = report
+        else:
+            return redirect(url_for('report_error'))
+                                                                      
 @app.route('/report_error', methods = ["GET, POST"])
 def report_error():                                                                        #выкинуть ошибку о неверном заполнении
     ...
