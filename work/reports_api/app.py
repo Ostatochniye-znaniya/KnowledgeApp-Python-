d8e826f8a5d18d8f25396eda24d343c772bbaabc
yar <yar2022789@gmail.com>
2026-04-19 19:54:59 +0300

    added api endpoints

--- work/reports_api/app.py
+++ work/reports_api/app.py
@@ -6,13 +6,73 @@ from models import Base, Users, Reports, Disciplines
 
 
 app = Flask(__name__)
+
 engine = create_engine(
     Config.db_url, 
     echo = True)
+
 Session = sessionmaker(bind = engine)
+session = Session()
 
 Base.metadata.create_all(engine)
+session.commit()
 
 @app.route('/', methods = ['GET'])
 def index():
     return "api для отчетов"
+
+
+@app.route('/reports', methods = ['GET', 'POST'])
+def reports():
+    if request.method == "GET":
+        result = session.query(Reports).all()
+        return [Reports.to_dict(x) for x in result]
+    
+    if request.method == "POST":
+        data = request.get_json()
+        try:
+            new_report = Reports(
+                report_id = data.get("report_id"),
+                discipline_id = data.get("discipline_id"),
+                teacher_id = data.get("teacher_id"),
+                file_path = data.get("file_path"),
+                is_correct = data.get("is_correct"),
+                done_in_paper_form = data.get("done_in_paper_form"),
+                done_in_electronic_form = data.get("done_in_electronic_form"),
+                all_done = data.get("all_done"),
+                semester_id = data.get("semester_id")
+            )
+
+            session.add(new_report)
+            session.commit()
+            return {'status':'done'}
+        except ValueError:
+            return 'Error'
+
+@app.route('/reports/done', methods = ['GET'])
+def done():
+    result = session.query(Reports).filter(Reports.all_done == True)
+    return [Reports.to_dict(x) for x in result]
+
+
+@app.route('/reports/<int:rep_id>', methods = ["GET", "PATCH"])
+def get_report(rep_id):
+    if request.method == "GET":
+        result = session.query(Reports).filter(Reports.report_id == rep_id)
+        return [Reports.to_dict(x) for x in result]
+    if request.method == "PATCH":
+        data = request.get_json()
+        report = session.get(Reports, rep_id)
+        if not report:
+            return jsonify({"error": "Report not found"}), 404
+        try:
+            for key, value in data.items():
+                if hasattr(report, key):
+                    setattr(report, key, value)
+            session.commit()
+            return jsonify(report.to_dict())
+        except Exception as e:
+            session.rollback()
+            return jsonify({"error": str(e)}), 400
+        
+        
--- work/reports_api/models.py
+++ work/reports_api/models.py
@@ -7,9 +7,9 @@ class Base(DeclarativeBase):
 class Reports(Base):
     __tablename__ = "reports"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
-    discipline_id: Mapped[int] = mapped_column(ForeignKey("disciplines.id"))
-    teacher_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    report_id: Mapped[int] = mapped_column(primary_key = True)
+    discipline_id: Mapped[int] = mapped_column(ForeignKey("disciplines.discipline_id"))
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
     file_path: Mapped[str] = mapped_column(String(255))
     is_correct: Mapped[bool] = mapped_column(Boolean)
     done_in_paper_form: Mapped[bool] = mapped_column(Boolean)
@@ -19,11 +19,16 @@ class Reports(Base):
 
     teacher: Mapped["Users"] = relationship(back_populates="reports")
     discipline: Mapped["Disciplines"] = relationship(back_populates="reports")
+    def to_dict(self):
+        return {
+        column.name: getattr(self, column.name)
+        for column in self.__table__.columns
+    }
 
 class Users(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
+    user_id: Mapped[int] = mapped_column(primary_key = True)
     name: Mapped[str] = mapped_column(String(255))
     email: Mapped[str] = mapped_column(String(255))
     password: Mapped[str] = mapped_column(String(255))
@@ -34,12 +39,24 @@ class Users(Base):
     reports: Mapped[list["Reports"]] = relationship(back_populates="teacher")
     disciplines: Mapped[list["Disciplines"]] = relationship(back_populates="teacher")
 
+    def to_dict(self):
+        return {
+        column.name: getattr(self, column.name)
+        for column in self.__table__.columns
+    }
+
 class Disciplines(Base):
     __tablename__ = "disciplines"
 
-    id: Mapped[int] = mapped_column(primary_key = True)
+    discipline_id: Mapped[int] = mapped_column(primary_key = True)
     name: Mapped[str] = mapped_column(String(255))
-    responsible_teacher_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    responsible_teacher_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
 
     teacher: Mapped["Users"] = relationship(back_populates="disciplines")
     reports: Mapped[list["Reports"]] = relationship(back_populates="discipline")
+    
+    def to_dict(self):
+        return {
+        column.name: getattr(self, column.name)
+        for column in self.__table__.columns
+    }
